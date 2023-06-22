3f2f01302c4c6b494091e21ab6f0d6e9f83d3e29
funny-family <maksvadik228@gmail.com>
2023-06-22 22:56:10 +0700

    update uplayground

--- playground/src/root.tsx
+++ playground/src/root.tsx
@@ -1,15 +1,20 @@
-import { onMount } from 'solid-js';
+import { Show, createSignal, onMount } from 'solid-js';
 import { Body, Head, Html, Meta, Scripts, Title } from 'solid-start';
 // import { Modal } from '../../dist/index.es';
 import { Modal } from '../../lib/modal';
 import type { ModalRootElement } from '../../lib/modal';
 import './root.css';
 
-export default function Root() {
+const Root = () => {
   let modelRef = undefined as any as ModalRootElement;
+  let anotherModalRef = undefined as any as ModalRootElement;
+
+  const { 0: isAnotherModalHidden, 1: setIsAnotherModalHiddenState } =
+    createSignal(false);
 
   onMount(() => {
-    console.log({ modelRef });
+    console.log('modelRef:', { modelRef });
+    console.log('anotherModalRef:', { anotherModalRef });
   });
 
   return (
@@ -20,21 +25,62 @@ export default function Root() {
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Body>
-        <button type="button" onClick={() => modelRef.showModal()}>
-          Open modal
-        </button>
+        <div>
+          <button type="button" onClick={() => modelRef.showModal()}>
+            Open modal
+          </button>
+          <Modal
+            class="modal"
+            ref={modelRef}
+            style={{ padding: '100px' }}
+            shouldCloseOnBackgroundClick={false}
+            // keepMounted={false}
+            onOpen={(event) => console.log('open event:', event)}
+            // onClose={(event) => console.log('close event:', event)}
+            // onCancel={(event) => console.log('cancel event:',event:)}
+            // onClick={(event:) => console.log('click event:', event)}
+            onBackdropClick={() => console.log('background click')}
+            children={
+              <div class="modal__content">
+                <h1>This is modal content!</h1>
+                <button type="button" onClick={() => modelRef.close()}>
+                  Close modal
+                </button>
+              </div>
+            }
+          />
+        </div>
 
-        <Modal class="modal" ref={modelRef}>
-          <div class="modal__content">
-            <h1>This is modal content!</h1>
-            <button type="button" onClick={() => modelRef.close()}>
-              Close modal
-            </button>
-          </div>
-        </Modal>
+        <div>
+          <button
+            type="button"
+            onClick={() => {
+              setIsAnotherModalHiddenState(true);
+              anotherModalRef.showModal();
+            }}
+          >
+            Open another modal
+          </button>
+          <Modal
+            ref={anotherModalRef}
+            shouldCloseOnBackgroundClick={true}
+            onClick={() => setIsAnotherModalHiddenState(false)}
+          >
+            <Show when={isAnotherModalHidden()} fallback={null}>
+              <div>
+                <h1>Another Modal!</h1>
+                <button type="button" onClick={() => anotherModalRef.close()}>
+                  Close another modal
+                </button>
+              </div>
+            </Show>
+          </Modal>
+        </div>
 
         <Scripts />
       </Body>
     </Html>
   );
-}
+};
+
+export default Root;
